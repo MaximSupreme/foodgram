698daa20390913f2eab817e164b4c2b6326239d7
Maxim Supreme <chernikovmaks2002@gmail.com>
2025-04-15 01:57:48 +0300

    Update files

--- backend/foodgram/api/mixins.py
+++ backend/foodgram/api/mixins.py
@@ -9,7 +9,6 @@ class AddDeleteRecipeMixin:
         recipe = self.get_object()
         user = request.user
         obj_exists = model.objects.filter(user=user, recipe=recipe).exists()
-        
         if request.method == 'POST':
             if obj_exists:
                 return Response(
@@ -26,13 +25,11 @@ class AddDeleteRecipeMixin:
                 },
                 status=status.HTTP_201_CREATED
             )
-            
         if not obj_exists:
             return Response(
                 {'errors': error_message['not_found']},
                 status=status.HTTP_400_BAD_REQUEST
             )
-            
         model.objects.filter(user=user, recipe=recipe).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
